d46e677ad125a558044a5f98c1c0764f4b59864d
nialljmiller <niall.j.miller@gmail.com>
2025-03-23 16:11:26 -0600

    auto update

--- cv_vis.py
+++ cv_vis.py
@@ -903,7 +903,8 @@ class PrimvsTessCrossMatch:
 
     # Modify run_pipeline to call populate_tess_sectors() after crossmatch:
     def run_pipeline(self):
-        crossmatch_path = self.output_dir + '/tess_proposal_targets.csv'
+        crossmatch_path = self.output_dir + '/tess_crossmatch_results.csv'
+        target_path = self.output_dir + '/tess_proposal_targets.csv'
         start_time = time.time()
         print("\n" + "="*80)
         print("RUNNING PRIMVS-TESS CROSS-MATCH PIPELINE")
@@ -917,7 +918,7 @@ class PrimvsTessCrossMatch:
         #self.download_tess_lightcurves()  # Optional: uncomment if needed
         self.generate_target_list()
         self.generate_summary_plots()
-        copy_target_npy_files(crossmatch_path)
+        copy_target_npy_files(target_path)
 
         #self.populate_tess_sectors_equatorial()
         end_time = time.time()
