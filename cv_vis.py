a2ebf467fb512b2b6e9c9d2f02aba3b759ba8dc1
nialljmiller <niall.j.miller@gmail.com>
2025-03-23 17:11:59 -0600

    auto update

--- cv_vis.py
+++ cv_vis.py
@@ -922,7 +922,7 @@ class PrimvsTessCrossMatch:
         target_list = pd.read_csv(target_path)
         star_names = target_list['sourceid'].values
 
-        get_lightcurves(target_pathtarget_path)
+        save_lightcurves_to_csv(target_pathtarget_path)
 
         #self.populate_tess_sectors_equatorial()
         end_time = time.time()
