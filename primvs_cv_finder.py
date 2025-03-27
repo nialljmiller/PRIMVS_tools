bcb60b0120b9b52fbcb23b6dc2d51b5f48afa7ed
nialljmiller <niall.j.miller@gmail.com>
2025-03-27 00:12:58 -0600

    auto update

--- primvs_cv_finder.py
+++ primvs_cv_finder.py
@@ -1079,7 +1079,7 @@ class PrimvsCVFinder:
         # Save the classifier probability directly
 
         # Only select candidates with probability >= 0.5
-        prob_threshold = 0.1
+        prob_threshold = 0.18
         high_prob_mask = self.filtered_data['cv_prob'] >= prob_threshold
         high_prob_count = high_prob_mask.sum()
         
